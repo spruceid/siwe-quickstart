920bf6c32162311f2ba980008b58d0fb4188fa78
Jacob <jacobbward0@gmail.com>
2022-01-13 22:53:44 +0000

    Small syntax changes for slightly simpler guide.

--- src/index.js
+++ src/index.js
@@ -20,16 +20,17 @@ ensAvatar.onerror = function () {
   ensAvatar.style.display = 'none' 
 }
 
+const domain = window.location.hostname
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 const signer = provider.getSigner()
-const domain = window.location.hostname
 
 function connectWallet () {
   provider.send('eth_requestAccounts', [])
-    .then(_ => {
+    .then(() => {
       connectWalletBtn.style.display = 'none'
       siweBtn.style.display = 'block'
-    }, () => console.log('user rejected request'))
+    })
+    .catch(() => console.log('user rejected request'))
 }
 
 async function checkWalletIsConnected () {
@@ -39,19 +40,8 @@ async function checkWalletIsConnected () {
   }
 }
 
-async function createSiweMessage (statement) {
-  return new SiweMessage({
-    domain,
-    address: await signer.getAddress(),
-    statement,
-    uri: 'http://' + domain,
-    version: '1',
-    chainId: '1'
-  }).signMessage()
-}
-
 async function signInWithEthereum () {
-  const message = await createSiweMessage('Sign in with Ethereum to the app.')
+  const message = createSiweMessage(await signer.getAddress(), 'Sign in with Ethereum to the app.')
 
   const signature = await signer.signMessage(message)
 
@@ -89,7 +79,7 @@ async function signInWithEthereum () {
 }
 
 async function load () {
-  const message = await createSiweMessage('Load your previously saved input.')
+  const message = createSiweMessage(await signer.getAddress(), 'Load your previously saved input.')
 
   const signature = await signer.signMessage(message)
 
@@ -112,7 +102,7 @@ async function load () {
 }
 
 async function save () {
-  const message = await createSiweMessage('Save your current input.')
+  const message = createSiweMessage(await signer.getAddress(), 'Save your current input.')
 
   const signature = await signer.signMessage(message)
 
@@ -130,6 +120,17 @@ async function save () {
   }
 
   window.fetch('http://localhost:8888', options)
+}
+
+function createSiweMessage (address, statement) {
+  return new SiweMessage({
+    domain,
+    address,
+    statement,
+    uri: 'http://' + domain,
+    version: '1',
+    chainId: '1'
+  }).signMessage()
 }
 
 checkWalletIsConnected()
