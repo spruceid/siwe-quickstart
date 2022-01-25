da50d2b919221941171340be2db58b3f80135107
K Rhoda <kelsey.rhoda@spruceid.com>
2022-01-25 16:38:10 -0700

    02 End: SIWE secured scratch pad app

--- 02_siwe_quickstart/src/app.js
+++ 02_siwe_quickstart/src/app.js
@@ -1,21 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+const siwe = require('siwe');
 
-function readFile(fileName) {
-  return fs.promises.readFile(fileName, { encoding: 'utf8' });
-}
-
-function writeFile(fileName, content) {
-  return fs.promises.writeFile(fileName, content, { encoding: 'utf8' });
-}
-
-function internalServerError(response) {
-  response
-    .writeHead(500, {
-      'Access-Control-Allow-Origin': '*',
-    })
-    .end();
-}
+const messageStore = {};
+const statements = {
+  load: 'Load your previously saved input.',
+  save: 'Save your current input.',
+  signIn: 'Sign-in with Ethereum.',
+  newMessage: 'Sign-in with Ethereum.',
+};
 
 // make sure the data directory exists
 try {
@@ -29,38 +22,133 @@ try {
   process.exit(1);
 }
 
+const defaultText = 'Greetings!';
 const port = 8888;
-const defaultFilePath = './data/text.txt';
 
-function load(response) {
-  readFile(defaultFilePath).then((content) => {
+function parseAndValidateSiweMessage(messageText, signature) {
+  const message = new siwe.SiweMessage(messageText);
+  if (messageText !== messageStore[message.nonce]) {
+    return Promise.reject(new Error('received message did not match stored message'));
+  }
+  message.signature = signature;
+  return message.validate();
+}
+
+function createSiweMessage(address, statement) {
+  const message = new siwe.SiweMessage({
+    domain: 'localhost',
+    address,
+    statement,
+    uri: 'http://localhost',
+    version: '1',
+    chainId: '1',
+  });
+  const messageText = message.signMessage();
+  const { nonce } = message;
+  messageStore[nonce] = messageText;
+  return messageText;
+}
+
+function addressToFileName(fileName) {
+  return `data/${fileName}.txt`;
+}
+
+function readFile(fileName) {
+  return fs.promises.readFile(fileName, { encoding: 'utf8' });
+}
+
+function writeFile(fileName, content) {
+  return fs.promises.writeFile(fileName, content, { encoding: 'utf8' });
+}
+
+function createDefaultFile(fileName) {
+  return fs.promises.writeFile(fileName, defaultText, { encoding: 'utf8' });
+}
+
+function internalServerError(response) {
+  response
+    .writeHead(500, {
+      'Access-Control-Allow-Origin': '*',
+    })
+    .end();
+}
+
+async function load(response, payload) {
+  try {
+    const message = await parseAndValidateSiweMessage(payload.message, payload.signature);
+    const fileName = await addressToFileName(message.address);
+    const content = await readFile(fileName);
     response
       .writeHead(200, {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'text/plain',
       })
       .end(content);
-  })
-    .catch((err) => {
-      console.error(err);
-      internalServerError(response);
-    });
+  } catch (err) {
+    console.error(err);
+    internalServerError(response);
+  }
 }
 
-function save(response, payload) {
-  const content = payload.content || '';
-  writeFile(defaultFilePath, content)
-    .then(() => {
-      response
-        .writeHead(200, {
-          'Access-Control-Allow-Origin': '*',
-        })
-        .end();
+async function save(response, payload) {
+  try {
+    const content = payload.content || '';
+    const message = await parseAndValidateSiweMessage(payload.message, payload.signature);
+    const fileName = await addressToFileName(message.address);
+    await writeFile(fileName, content);
+    response
+      .writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+      })
+      .end();
+  } catch (err) {
+    console.error(err);
+    internalServerError(response);
+  }
+}
+
+async function signIn(response, payload) {
+  try {
+    const message = await parseAndValidateSiweMessage(payload.message, payload.signature);
+    const fileName = addressToFileName(message.address);
+    const content = await fs.promises.access(fileName)
+      .then(
+        () => readFile(fileName),
+        () => createDefaultFile(fileName).then(() => defaultText),
+      );
+
+    response
+      .writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+      })
+      .end(content);
+  } catch (err) {
+    console.error(err);
+    internalServerError(response);
+  }
+}
+
+function newMessage(response, payload) {
+  const statementKey = payload?.statement;
+  const statement = statementKey && statements[statementKey];
+
+  if (statement) {
+    const message = createSiweMessage(payload.address, statement);
+    response
+      .writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*',
+      })
+      .end(message);
+    return;
+  }
+
+  response
+    .writeHead(400, {
+      'Access-Control-Allow-Origin': '*',
     })
-    .catch((err) => {
-      console.error(err);
-      internalServerError(response);
-    });
+    .end();
 }
 
 http.createServer((request, response) => {
@@ -77,22 +165,34 @@ http.createServer((request, response) => {
     request.on('end', () => {
       const payload = JSON.parse(body);
 
-      // if the action is load, read the data currently in the
-      // local file and send it to the frontend
-      if (payload.action === 'load') {
-        load(response, payload);
+      switch (payload.action) {
+        case 'newMessage':
+          // if the action is newMessage, read the action for which
+          // the message will be signed and generate the message.
+          newMessage(response, payload);
+          return;
 
-      // if the action is save, store the content in the local file
-      } else if (payload.action === 'save') {
-        save(response, payload);
-
-      // reject any other actions
-      } else {
-        response
-          .writeHead(400, {
-            'Access-Control-Allow-Origin': '*',
-          })
-          .end();
+        case 'load':
+          // if the action is load, read the data currently in the
+          // local file and send it to the frontend
+          load(response, payload);
+          return;
+        case 'save':
+          // if the action is save, store the content in the local file
+          save(response, payload);
+          return;
+        case 'signIn':
+          // if the action is signIn, create the default file if the user
+          // does not already have a file, and then send the content of
+          // the file to the frontend
+          signIn(response, payload);
+          return;
+        default:
+          response
+            .writeHead(400, {
+              'Access-Control-Allow-Origin': '*',
+            })
+            .end();
       }
     });
   }
