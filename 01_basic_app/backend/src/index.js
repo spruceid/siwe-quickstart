1414619e14a8c572df70e87bfdd1069d1aa7d078
Simon Bihel <simon.bihel@spruceid.com>
2022-01-27 16:12:19 +0000

    Basic app

--- 01_basic_app/backend/src/index.js
+++ 01_basic_app/backend/src/index.js
@@ -7,15 +7,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
+app.get('/nonce', function(_, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.send(generateNonce());
+})
+
 app.post('/verify', function(req, res) {
     const { message, signature } = req.body;
     const siweMessage = new SiweMessage(message);
     siweMessage.signature = signature;
-    res.send(siweMessage.validate());
-})
-
-app.get('/nonce', function(_, res) {
-    res.send(generateNonce());
+    try {
+        siweMessage.validate();
+        res.send(true);
+    } catch {
+        res.send(false);
+    }
 })
 
 app.listen(3000);
--- 01_basic_app/frontend/src/index.js
+++ 01_basic_app/frontend/src/index.js
@@ -1,54 +1,59 @@
-import { ethers } from 'ethers'
-import { SiweMessage } from 'siwe'
+import { ethers } from 'ethers';
+import { SiweMessage } from 'siwe';
 
-const domain = window.location.host
-const origin = window.location.origin
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-const signer = provider.getSigner()
+const domain = window.location.host;
+const origin = window.location.origin;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
 
-let message = null;
-let signature = null;
+const BACKEND_ADDR = "http://localhost:3000";
 
-function createSiweMessage(address, statement) {
+async function createSiweMessage(address, statement) {
+    const res = await fetch(`${BACKEND_ADDR}/nonce`);
     const message = new SiweMessage({
         domain,
         address,
         statement,
         uri: origin,
         version: '1',
-        chainId: '1'
+        chainId: '1',
+        nonce: await res.text()
     });
     return message.signMessage();
 }
 
 function connectWallet() {
     provider.send('eth_requestAccounts', [])
-        .catch(() => console.log('user rejected request'))
+        .catch(() => console.log('user rejected request'));
 }
 
+let message = null;
+let signature = null;
+
 async function signInWithEthereum() {
-    message = createSiweMessage(
+    message = await createSiweMessage(
         await signer.getAddress(),
         'Sign in with Ethereum to the app.'
-    )
+    );
+    console.log(message);
     signature = await signer.signMessage(message);
-    console.log(signature)
+    console.log(signature);
 }
 
 async function sendForVerification() {
-    const res = await fetch("http://localhost:3000/verify", {
+    const res = await fetch(`${BACKEND_ADDR}/verify`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ message, signature }),
     });
-    console.log(res);
+    console.log(await res.text());
 }
 
-const connectWalletBtn = document.getElementById('connectWalletBtn')
-const siweBtn = document.getElementById('siweBtn')
-const verifyBtn = document.getElementById('verifyBtn')
-connectWalletBtn.onclick = connectWallet
-siweBtn.onclick = signInWithEthereum
-verifyBtn.onclick = sendForVerification
+const connectWalletBtn = document.getElementById('connectWalletBtn');
+const siweBtn = document.getElementById('siweBtn');
+const verifyBtn = document.getElementById('verifyBtn');
+connectWalletBtn.onclick = connectWallet;
+siweBtn.onclick = signInWithEthereum;
+verifyBtn.onclick = sendForVerification;
