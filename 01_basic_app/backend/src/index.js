754c5040baf41e0a54f991ac7d7c7b6137fd29e8
Simon Bihel <sbihel@users.noreply.github.com>
2022-01-27 16:35:27 +0000

    Start rework of the guides (#9)

---
+++ 01_basic_app/backend/src/index.js
@@ -0,0 +1,27 @@
+import { SiweMessage, generateNonce } from 'siwe';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/nonce', function(_, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.send(generateNonce());
+})
+
+app.post('/verify', function(req, res) {
+    const { message, signature } = req.body;
+    const siweMessage = new SiweMessage(message);
+    siweMessage.signature = signature;
+    try {
+        siweMessage.validate();
+        res.send(true);
+    } catch {
+        res.send(false);
+    }
+})
+
+app.listen(3000);
