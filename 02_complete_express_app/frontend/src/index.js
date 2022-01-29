7ad9db8793ee78bb3f8680e0330f9128dab31937
Simon Bihel <sbihel@users.noreply.github.com>
2022-01-29 00:29:38 +0000

    Further rework (#10)
    * WIP
    
    * Fix

---
+++ 02_complete_express_app/frontend/src/index.js
@@ -0,0 +1,62 @@
+import { ethers } from 'ethers';
+import { SiweMessage } from 'siwe';
+
+const domain = window.location.host;
+const origin = window.location.origin;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+const BACKEND_ADDR = "http://localhost:3000";
+
+async function createSiweMessage(address, statement) {
+    const res = await fetch(`${BACKEND_ADDR}/nonce`, {
+        credentials: 'include',
+    });
+    const message = new SiweMessage({
+        domain,
+        address,
+        statement,
+        uri: origin,
+        version: '1',
+        chainId: '1',
+        nonce: await res.text()
+    });
+    return message.signMessage();
+}
+
+function connectWallet() {
+    provider.send('eth_requestAccounts', [])
+        .catch(() => console.log('user rejected request'));
+}
+
+async function signInWithEthereum() {
+    const message = await createSiweMessage(
+        await signer.getAddress(),
+        'Sign in with Ethereum to the app.'
+    );
+    const signature = await signer.signMessage(message);
+
+    const res = await fetch(`${BACKEND_ADDR}/sign_in`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, signature }),
+        credentials: 'include'
+    });
+    console.log(await res.text());
+}
+
+async function getInformation() {
+    const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
+        credentials: 'include',
+    });
+    console.log(await res.text());
+}
+
+const connectWalletBtn = document.getElementById('connectWalletBtn');
+const siweBtn = document.getElementById('siweBtn');
+const infoBtn = document.getElementById('infoBtn');
+connectWalletBtn.onclick = connectWallet;
+siweBtn.onclick = signInWithEthereum;
+infoBtn.onclick = getInformation;
