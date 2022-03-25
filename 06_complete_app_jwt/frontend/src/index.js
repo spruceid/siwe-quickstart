7941e249568c953ce8ce98936f98fcd57fa14047
Dante Monaldo <dante.monaldo@gmail.com>
2022-03-24 22:48:24 -0700

    jwt cookie frontend

--- 06_complete_app_jwt/frontend/src/index.js
+++ 06_complete_app_jwt/frontend/src/index.js
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 import { SiweMessage } from 'siwe';
+import jwt_decode from 'jwt-decode';
 
 const domain = window.location.host;
 const origin = window.location.origin;
@@ -11,6 +12,8 @@ async function createSiweMessage(address, statement) {
     const res = await fetch(`${BACKEND_ADDR}/nonce`, {
         credentials: 'include',
     });
+    const nonce_jwt = await res.text()
+    const decoded = jwt_decode(nonce_jwt)
     const message = new SiweMessage({
         domain,
         address,
@@ -18,9 +21,9 @@ async function createSiweMessage(address, statement) {
         uri: origin,
         version: '1',
         chainId: '1',
-        nonce: await res.text()
-    });
-    return message.prepareMessage();
+        nonce: decoded.nonce
+    }).prepareMessage();
+    return { message, nonce_jwt };
 }
 
 function connectWallet() {
@@ -29,7 +32,7 @@ function connectWallet() {
 }
 
 async function signInWithEthereum() {
-    const message = await createSiweMessage(
+    const { message, nonce_jwt } = await createSiweMessage(
         await signer.getAddress(),
         'Sign in with Ethereum to the app.'
     );
@@ -40,7 +43,7 @@ async function signInWithEthereum() {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message, signature }),
+        body: JSON.stringify({ message, signature, nonce_jwt }),
         credentials: 'include'
     });
     console.log(await res.text());
@@ -48,7 +51,7 @@ async function signInWithEthereum() {
 
 async function getInformation() {
     const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
-        credentials: 'include',
+        credentials: 'include'
     });
     console.log(await res.text());
 }
