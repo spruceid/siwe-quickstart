810ad5539f881fcaa59a250efb4c64d3ef46cea8
ZakesGit <Zakhelecrypto@gmail.com>
2024-08-09 18:30:49 +0200

    Create index2.js

---
+++ 00_print/src/index2.js
@@ -0,0 +1,23 @@
+src/index.js
+
+const siwe = require('siwe');
+
+const domain = "localhost";
+const origin = "https://localhost/login";
+
+function createSiweMessage (address, statement) {
+  const siweMessage = new siwe.SiweMessage({
+    domain,
+    address,
+    statement,
+    uri: origin,
+    version: '1',
+    chainId: '1'
+  });
+  return siweMessage.prepareMessage();
+}
+
+console.log(createSiweMessage(
+    "0x6Ee9894c677EFa1c56392e5E7533DE76004C8D94",
+    "This is a test statement."
+  ));
